4e31b246b5578de5517a796a3b7dc34df4cce5bb
Ammar Fatwa <ammar.fatwa.ft21@mail.umy.ac.id>
2022-11-02 15:39:41 +0700

    add class

--- Exercise2Algo/Program.cs
+++ Exercise2Algo/Program.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace Exercise2Algo
 {
-    internal class Program
+   class Program
     {
+
+        //array of integers to hold values
+        private int[] ammar = new int[35];
+        private int camp_count = 0;
+        private int mov_count = 0;
+
+        //number of elemebt in array 
+        private int n;
         static void Main(string[] args)
         {
         }
